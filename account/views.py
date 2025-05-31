f69d587a90b245a051f9ff9eee652d2e7402b2e9
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-05-31 17:42:00 -0600

    feat: added section for editing personal data

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 def user_login(request):
@@ -46,6 +47,10 @@ def register(request):
 
             # save the User object
             new_user.save()
+
+            # create the user profile
+            Profile.objects.create(user=new_user)
+
             return render(
                 request,
                 'account/register_done.html',
@@ -66,4 +71,30 @@ def dashboard(request):
         request,
         'account/dashboard.html',
         {'section': 'dashboard'}
+    )
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user, data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form,
+        }
     )
